7fc1bffdc71388983b47d1c54b64fee27e6451e4
sky321 <github.10.csw@spamgourmet.com>
2019-03-12 17:55:41 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,6 +9,7 @@ fi
 # in Docker, the USER variable is unset by default
 # but some programs (like jack toolchain) rely on it
 export USER="$(whoami)"
+export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
 
 # Launch screen session
 screen -s /bin/bash
